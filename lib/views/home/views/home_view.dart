b14b511f5ca2ccce7d6fb4ca5b856dc7147b14aa
Omar Yehia <omaryahiatawfiek@gmail.com>
2024-06-25 14:51:10 +0300

    fix `FocusScope` in `SearchBar`

--- lib/views/home/views/home_view.dart
+++ lib/views/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../components/keyboard_dismissal.dart';
 import '../../../generated/localization.dart';
 import '../../../models/post/post.dart';
 import '../widgets/post_card.dart';
@@ -29,45 +30,47 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return NestedScrollView(
-      headerSliverBuilder: (context, value) {
-        return [
-          SliverToBoxAdapter(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 2.0,
-                    offset: const Offset(2.0, 2.0),
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              child: SizedBox(
-                width: 180,
-                child: TabBar(
-                  controller: _tabController,
-                  labelColor: Colors.black,
-                  unselectedLabelColor: Colors.grey.shade600,
-                  indicatorColor: Theme.of(context).colorScheme.secondary,
-                  tabs: [
-                    Tab(text: LocaleKeys.home.tr()),
-                    Tab(text: LocaleKeys.popular.tr()),
+    return KeyboardDismissal(
+      child: NestedScrollView(
+        headerSliverBuilder: (context, value) {
+          return [
+            SliverToBoxAdapter(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.surface,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 2.0,
+                      offset: const Offset(2.0, 2.0),
+                    ),
                   ],
+                ),
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 180,
+                  child: TabBar(
+                    controller: _tabController,
+                    labelColor: Colors.black,
+                    unselectedLabelColor: Colors.grey.shade600,
+                    indicatorColor: Theme.of(context).colorScheme.secondary,
+                    tabs: [
+                      Tab(text: LocaleKeys.home.tr()),
+                      Tab(text: LocaleKeys.popular.tr()),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ];
-      },
-      body: TabBarView(
-        controller: _tabController,
-        children: const [
-          HomeTabView(),
-          PopularTabView(),
-        ],
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController,
+          children: const [
+            HomeTabView(),
+            PopularTabView(),
+          ],
+        ),
       ),
     );
   }
